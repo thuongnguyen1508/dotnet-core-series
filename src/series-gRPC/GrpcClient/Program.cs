b6de6c03682f31dae5f95b3017121c707268de0d
Nguyễn Văn Thương <80138135+thuongnguyen1508@users.noreply.github.com>
2023-07-06 15:15:19 +0700

    Series gRPC (#3)
    * init gRPC
    
    * set up gRPC client
    
    ---------
    
    Co-authored-by: Thuong Nguyen <thuong.nguyen@muaban.net>

---
+++ src/series-gRPC/GrpcClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using GrpcServiceDemo;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "hh" });
+
+            Console.WriteLine(reply);
+            Console.WriteLine("Press any key to exit ... ");
+            Console.ReadLine();
+        }
+    }
+}
